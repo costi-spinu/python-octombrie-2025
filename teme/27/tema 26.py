b7b366d376bb09ae18a47ce4b06e820d9c629a97
Spinu Costelus <costyspinu@gmail.com>
2025-10-16 20:13:42 +0300

    Update

---
+++ teme/27/tema 26.py
@@ -0,0 +1,9 @@
+# Task
+# Create a project and its repository. The project should contain the following files:
+# "file1.cod", "file1.exe", [commit 1]
+# "file2.cod", "file2.exe", [commit 2]
+# "file3.cod", "file3.tmp", [commit 3]
+# "file4.cod", "file4.obj", [commit 4]
+# Each group of files is added with a separate commit, indicated next to the files.
+# The repository should ignore the files ".exe", ".tmp", ".obj".
+# Attach screenshots of the commit history and repository contents to the homework report.
